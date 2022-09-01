c7503088629a63232883596c931418ad2b549f9c
Bastien Gorissen <kadomony@gmail.com>
2022-09-01 15:59:38 +0200

    Move towards waypoints

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -8,15 +8,34 @@ public class EnemyBehaviour : MonoBehaviour
     
     private NavMeshAgent agent;
     public Transform playerTransform;
+    private Transform waypointTransform;
+    public Transform groupTransform;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        GetNewDestination();
     }
+
+    void GetNewDestination() {
+        Transform currentDestination = waypointTransform;
+
+        do {
+            int index = Random.Range(0, groupTransform.childCount);
+            waypointTransform = groupTransform.GetChild(index);
+        } while(waypointTransform == currentDestination);
+
+        agent.SetDestination(waypointTransform.position);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        print("In trigger!");
+        GetNewDestination();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(playerTransform.position);
     }
 }
