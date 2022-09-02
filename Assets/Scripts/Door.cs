45bc60d161b43be240da9ac8660a7808ab1cb879
Thomas Stassin <thomas.stassin@gsmproductions.com>
2022-09-02 16:38:16 +0200

    final door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Door : MonoBehaviour
 {
@@ -9,23 +10,47 @@ public class Door : MonoBehaviour
     private Vector3 closedPositon;
 
     public float doorTime = 1f;
+    public float openTime = 2f;
+
+    public UnityEvent whenOpen;
+    public UnityEvent whenClosed;
 
 
-    public enum State {open, closed, opening}
+    public enum State {open, closed, opening, closing}
     public State status = State.closed;
 
     IEnumerator OpeningDoor(){
         status = State.opening;
+        Vector3 startPosition = transform.position;
         float time = 0f;
         while(time < doorTime){
             time += Time.deltaTime;
             float ratio = time/doorTime;
-            transform.position = Vector3.Lerp(closedPositon, openPosition, ratio); 
+            transform.position = Vector3.Lerp(startPosition, openPosition, ratio); 
             yield return null;
         }
 
         transform.position = openPosition;
         status = State.open;
+        whenOpen.Invoke();
+
+        yield return new WaitForSeconds(openTime);
+        CloseDoor();
+    }
+
+        IEnumerator ClosingDoor(){
+        status = State.closing;
+        float time = 0f;
+        while(time < doorTime){
+            time += Time.deltaTime;
+            float ratio = time/doorTime;
+            transform.position = Vector3.Lerp(openPosition, closedPositon, ratio); 
+            yield return null;
+        }
+
+        transform.position = closedPositon;
+        status = State.closed;
+        whenClosed.Invoke();
     }
 
     // Start is called before the first frame update
@@ -46,8 +71,7 @@ public class Door : MonoBehaviour
     }
 
     private void CloseDoor(){
-        transform.position = closedPositon;
-        status = State.closed;
+        StartCoroutine(ClosingDoor());
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -55,8 +79,9 @@ public class Door : MonoBehaviour
             if(status == State.closed) {
                 OpenDoor();
                 }
-            else if(status == State.open){
-                CloseDoor();
+            else if(status == State.closing){
+                StopAllCoroutines();
+                OpenDoor();
             }
         }
     }
